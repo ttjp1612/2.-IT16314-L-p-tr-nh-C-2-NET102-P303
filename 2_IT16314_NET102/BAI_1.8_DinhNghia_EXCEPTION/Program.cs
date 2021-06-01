7ae2c860a251c74f88646f4e0cf36b70e812e8cc
Nguyễn Anh Dũng <dungna29@fpt.edu.vn>
2021-06-01 11:43:15 +0700

    1

---
+++ 2_IT16314_NET102/BAI_1.8_DinhNghia_EXCEPTION/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace BAI_1._8_DinhNghia_EXCEPTION
+{
+    class Program
+    {
+        #region Phần 1: Định nghĩa ra 1 EXCEPTION bên trong 1 phương thức
+
+        static void dangkyClubCsharp(string ten, int tuoi)
+        {
+            if (string.IsNullOrEmpty(ten))
+            {
+                Exception exception = new Exception("Tên không để null nhé");
+                throw exception;
+            }
+
+            if (tuoi <18)
+            {
+                throw new Exception("Bạn chưa đủ tuổi vào Club C# nhé");
+            }
+            Console.WriteLine("Chào mừng bạn đến với Club C# " + ten + " " + tuoi);
+        }
+
+
+        #endregion
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding("UTF-8");
+            try
+            {
+                dangkyClubCsharp("Dung", 17);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+}
---
+++ 2_IT16314_NET102/BAI_2.2_CLASS_GENERIC/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace BAI_2._2_CLASS_GENERIC
+{
+    class Program
+    {
+        /*
+         *  Lớp Generic         *
+         *  - Trước thì chúng ta làm việc với các kiểu dữ liệu tường minh nay khi học được Generic chúng ta chỉ quan tâm T cũng là 1 dạng kiểu dữ liệu nhưng do người sử dụng định nghĩa sẽ giúp tái sử dụng hơn rất nhiều.
+         *  - Khi truyền kiểu dữ liệu cho T thì nó sẽ thay thế toàn bộ các chỗ gọi đến T thành các kiểu dữ liệu......
+         *  - Giúp định nghĩa một thao tác dữ liệu với kiểu dữ liệu chung nhất nhìn hạn chế viết code và tái sử dụng.          
+           
+         */
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding("UTF-8");
+        }
+    }
+}
